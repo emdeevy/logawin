f9e9cc455f345f1633d0903cb24a3880069ba4db
Denis Makula <denis@emdeevy.me>
2023-10-02 01:42:58 +0000

    document Logger

--- src/Logawin/Logger.php
+++ src/Logawin/Logger.php
@@ -4,10 +4,24 @@ namespace Logawin;
 
 use Stringable;
 
+/**
+ * class Logger
+ *
+ * This class implements the LoggerInterface for logging messages.
+ *
+ * @package Logawin
+ */
 class Logger implements LoggerInterface
 {
+    /**
+     * Logs a message.
+     *
+     * @param string|Stringable $message The message to be logged.
+     *
+     * @return void
+     */
     public function log(Stringable|string $message): void
     {
-        printf("Logger: %s\n", $message);
+        printf("[Console]: %s\n", $message);
     }
 }
