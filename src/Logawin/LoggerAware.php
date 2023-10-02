dfd0b2aa981e2ff76562034219f7fe703cffe562
Denis Makula <denis@emdeevy.me>
2023-10-02 01:35:21 +0000

    document LoggerAware

--- src/Logawin/LoggerAware.php
+++ src/Logawin/LoggerAware.php
@@ -2,15 +2,36 @@
 
 namespace Logawin;
 
+/**
+ * Trait LoggerAware
+ *
+ * This trait provides functionality for classes that need to be aware of a logger.
+ *
+ * @package Logawin
+ */
 trait LoggerAware
 {
+    /**
+     * @var LoggerInterface The logger instance.
+     */
     private LoggerInterface $Logger;
 
+    /**
+     * Set the logger instance.
+     *
+     * @param LoggerInterface $Logger The logger instance to be set.
+     * @return void
+     */
     public function setLogger(LoggerInterface $Logger): void
     {
         $this->Logger = $Logger;
     }
 
+    /**
+     * Get the logger instance.
+     *
+     * @return LoggerInterface The logger instance.
+     */
     public function getLogger(): LoggerInterface
     {
         if (!isset($this->Logger)) {
